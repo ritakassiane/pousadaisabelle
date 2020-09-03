c90c0a8ffceeeced07cb1fe598fd93fa30862673
Rita Kassiane <ritakassiane.t.i@gmail.com>
2020-09-03 02:10:04 -0300

    address function with API CEP

--- v1/data-base-csv.py
+++ v1/data-base-csv.py
@@ -1,3 +1,17 @@
 # This file put together all the functions that will be used for data manipulation.
 
 import pandas as pd 
+import requests
+
+# The function recives a CEP and returns a dict with data about address
+def address_data(cep):
+    if len(cep) != 8:
+        print('CEP inválido!')
+        cep = input('Digite novamente:')
+    r = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
+    address = r.json()
+    return address
+
+    
+
+
---
+++ v1/teste_dict.py
@@ -0,0 +1,4 @@
+lista = []
+dicionariosimples = {'Ana': 2, 'Lucas':0}
+lista.append(dicionariosimples)
+print(lista[0])
